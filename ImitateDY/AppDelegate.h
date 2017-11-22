0f5a38c5a1fb7f2e180e528786650dc5d12f96e5
yangwei <yangw@nextechat.com>
2017-11-22 16:26:51 +0800

    Initial Commit

---
+++ ImitateDY/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImitateDY
+//
+//  Created by YangWei on 2017/11/22.
+//  Copyright © 2017年 Apple-YangWei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImitateDY/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImitateDY
+//
+//  Created by YangWei on 2017/11/22.
+//  Copyright © 2017年 Apple-YangWei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
